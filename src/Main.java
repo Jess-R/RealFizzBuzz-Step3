b8ca0431ff0ab75e981d1f362ef46b4cdc2e5c26
Jessica <jwrisavi@gmail.com>
2020-11-24 14:09:26 -0700

    Added logic to calculate word count totals

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 	public static boolean containsDigit(int number, int digit) {
 		while (number > 0) {
@@ -9,20 +11,39 @@ public class Main {
 		return false;
 	}
 	public static void main(String[] args) {
-		int number = 30;
+		Scanner sc = new Scanner(System.in);
+		System.out.print(("Assuming you start at 1, enter an integer for a max range: "));
+		int number = sc.nextInt();
+
+		int fizzCount = 0;
+		int buzzCount = 0;
+		int fizzBuzzCount = 0;
+		int luckyCount = 0;
+		int unalteredCount = 0;
 
 		for (int n = 1; n <= number; n++) {
 			if (n == 3 || containsDigit(n, 3)) {
 				System.out.print("lucky ");
+				luckyCount += 1;
 			} else if (n % 15 == 0) {
 				System.out.print("fizzbuzz ");
+				fizzBuzzCount += 1;
 			} else if (n % 3 == 0) {
 				System.out.print("fizz ");
+				fizzCount += 1;
 			} else if (n % 5 == 0) {
 				System.out.print("buzz ");
+				buzzCount += 1;
 			} else {
 				System.out.print(n + " ");
+				unalteredCount += 1;
 			}
 		}
+
+		System.out.print("\nFizz: " + fizzCount);
+		System.out.print("\nBuzz: " + buzzCount);
+		System.out.print("\nFizzBuzz: " + fizzBuzzCount);
+		System.out.print("\nLucky: " + luckyCount);
+		System.out.print("\nInteger: " + unalteredCount);
 	}
 }
