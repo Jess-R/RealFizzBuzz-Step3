779e2e64915f557bad291461cb91542a53c449be
Jessica <jwrisavi@gmail.com>
2020-11-24 22:02:57 -0700

    refactoring class and unit tests

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-	public static boolean containsDigit(int number, int digit) {
-		while (number > 0) {
-			if (number % 10 == digit) {
-				return true;
-			}
-			number = number / 10;
-		}
-		return false;
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print(("Assuming you start at 1, enter an integer for a max range: "));
-		int number = sc.nextInt();
 
+	public static void run(int number) {
 		int fizzCount = 0;
 		int buzzCount = 0;
 		int fizzBuzzCount = 0;
@@ -22,7 +10,7 @@ public class Main {
 		int unalteredCount = 0;
 
 		for (int n = 1; n <= number; n++) {
-			if (n == 3 || containsDigit(n, 3)) {
+			if (Integer.toString(n).contains("3")) {
 				System.out.print("lucky ");
 				luckyCount += 1;
 			} else if (n % 15 == 0) {
@@ -45,5 +33,21 @@ public class Main {
 		System.out.print("\nFizzBuzz: " + fizzBuzzCount);
 		System.out.print("\nLucky: " + luckyCount);
 		System.out.print("\nInteger: " + unalteredCount);
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number;
+
+		do {
+			System.out.print(("Assuming you start at 1, enter a whole number for a max range: "));
+			while (!sc.hasNextInt()) {
+				System.out.print("That is not a whole number. Try again: ");
+				sc.next();
+			}
+			number = sc.nextInt();
+		} while (number <= 0);
+
+		run(number);
 	}
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,21 +1,40 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
 	@Test
-	public void testContainsDigit() {
+	public void testRun() {
 		Main tester = new Main();
-		int number1 = 30;
+
+		int number1 = 43;
 		int number2 = 15;
-		int digit = 3;
+		int number3 = 20;
+		int number4 = 18;
+		int number5 = 13;
 
-		assertTrue(tester.containsDigit(number1, digit));
-		assertFalse(tester.containsDigit(number2, digit));
-	}
+		assertTrue(Integer.toString(number1).contains("3"));
+		assertFalse(Integer.toString(number2).contains("3"));
 
-	@Test
-	public void main() {
+		// test modulus 15 logic:
+		int result1 = number1 % 15;
+		int result2 = number2 % 15;
+
+		assertNotEquals(0, result1);
+		assertEquals(0, result2);
+
+		// test modulus 3 logic
+		int result3 = number3 % 3;
+		int result4 = number4 % 3;
+
+		assertNotEquals(0, result3);
+		assertEquals(0, result4);
+
+		// test modulus 5 logic
+		int result5 = number5 % 5;
+		int result6 = number3 % 5;
+
+		assertNotEquals(0, result5);
+		assertEquals(0, result6);
 	}
 }
